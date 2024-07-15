baa653452f96001eb5d9e5d3dbb26fd2d113d794
frankPerez12bo <frankperez7777pc@gmail.com>
2024-07-15 18:23:54 -0500

    Add files via upload

---
+++ semana13ma/src/clases/usuario.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package clases;
+
+/**
+ *
+ * @author PC13-LAB01
+ */
+public class usuario {
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getContra() {
+        return contra;
+    }
+
+    public void setContra(int contra) {
+        this.contra = contra;
+    }
+
+    public usuario() {
+    }
+    private String nombre;
+    private int contra;
+    
+}
---
+++ semana13ma/src/semana13ma/color.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package semana13ma;
+
+import java.awt.Color;
+
+/**
+ *
+ * @author PC13-LAB01
+ */
+public class color extends Color {
+
+    public color(int i, int i0, int i1) {
+    }
+    
+}
