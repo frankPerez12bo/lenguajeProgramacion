03e08b931070acc7f8abad21c5e0efccf6d28861
frankPerez12bo <frankperez7777pc@gmail.com>
2024-07-17 11:44:20 -0600

    Add files via upload

---
+++ CAplica02/src/nuevo/nn.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package nuevo;
+
+/**
+ *
+ * @author Usuario
+ */
+public class nn {
+    private String contra;
+    private String nombre;
+
+    public nn() {
+    }
+
+    public String getContra() {
+        return contra;
+    }
+
+    public void setContra(String contra) {
+        this.contra = contra;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    
+}
